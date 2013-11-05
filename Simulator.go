5bd5ee24659e388b355f8ef331606afe33330022
Ben Echols <ben.echols@webfilings.com>
2013-11-04 20:23:33 -0700

    Just a counple lines of the simulator

--- Simulator.go
+++ Simulator.go
@@ -3,12 +3,14 @@ package main
 import "time"
 
 type SolarSimulator struct {
-	entities    map[int32]*Entity
+	entities    map[int32]*Entity // Anything that can collide in space
+	characters  map[int32]*Entity // Things that can go inside of ships
 	next_update time.Time
 }
 
 func (ss *SolarSimulator) runSimulation(update chan EntityUpdate, login chan Entity, quit chan int) {
+	//
 	for {
-
+		//time.After(time.Millisecond)
 	}
 }
